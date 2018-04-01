d05179e4d8a5cf2a2d6370da510eb1f684b2a53e
flylzj <1449902124@qq.com>
2018-04-01 18:51:43 +0800

    环境变量bug

--- main.py
+++ main.py
@@ -71,13 +71,13 @@ def main():
     m = '0-59/' + str(randint(5,10))
     scheduler = BlockingScheduler()
     scheduler.add_job(t.get_and_dom, 'cron', hour='0-6', minute='0-59/20')  # 储存
-    scheduler.add_job(t.forward, 'cron', hour='0-23/2', minute='0-59/1')  # 转发
-    # scheduler.add_job(t.send_origin, 'cron', args=(one_word, "一言", "one_word"), hour=8, minute=1)  # 一言
-    # scheduler.add_job(t.send_origin, 'cron', args=(get_weather, "天气", "weather"), hour=6, minute=1)  # 天气
-    # scheduler.add_job(t.send_origin, 'cron', args=(mie_word, "咩语", "mie_word"), hour=22, minute=1)  # 咩语
-    # scheduler.add_job(t.send_origin, 'cron', args=(recommend_food, "美食推荐", "food"), hour=11, minute=1)  # 美食推荐
-    # scheduler.add_job(t.send_origin, 'cron', args=(daily_news, "每日国际视野", "news"), hour=9, minute=1)  # 每日国际视野
-    # scheduler.add_job(t.send_origin, 'cron', args=(history_of_today, "历史上的今天", "history"), hour=10, minute=1)  # 历史上的今天
+    scheduler.add_job(t.forward, 'cron', hour='0-23/2', minute=m)  # 转发
+    scheduler.add_job(t.send_origin, 'cron', args=(one_word, "一言", "one_word"), hour=8, minute=1)  # 一言
+    scheduler.add_job(t.send_origin, 'cron', args=(get_weather, "天气", "weather"), hour=6, minute=1)  # 天气
+    scheduler.add_job(t.send_origin, 'cron', args=(mie_word, "咩语", "mie_word"), hour=22, minute=1)  # 咩语
+    scheduler.add_job(t.send_origin, 'cron', args=(recommend_food, "美食推荐", "food"), hour=11, minute=1)  # 美食推荐
+    scheduler.add_job(t.send_origin, 'cron', args=(daily_news, "每日国际视野", "news"), hour=9, minute=1)  # 每日国际视野
+    scheduler.add_job(t.send_origin, 'cron', args=(history_of_today, "历史上的今天", "history"), hour=10, minute=1)  # 历史上的今天
     scheduler.start()
 
 
